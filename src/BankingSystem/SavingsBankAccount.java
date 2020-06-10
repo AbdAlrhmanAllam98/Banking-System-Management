b397d3df318c7839bdba8169a30414df3efb8290
Abd Alrhman Allam <62191765+AbdAlrhmanAllam98@users.noreply.github.com>
2020-06-11 01:53:34 +0200

    VIP Account with new featrues

---
+++ src/BankingSystem/SavingsBankAccount.java
@@ -0,0 +1,44 @@
+package BankingSystem;
+
+public class SavingsBankAccount extends BankAccount {
+ 
+    double minimumBalance;
+
+    public SavingsBankAccount(double minimumBalance, double balance) {
+        super(balance);
+        this.minimumBalance = minimumBalance;        
+   }
+   public SavingsBankAccount(){
+       super(1000);
+       minimumBalance=1000;
+   }
+    @Override
+   public boolean withdraw(double amount){
+       if(balance-amount<minimumBalance){
+           System.out.println("error invalid input");
+           return false;
+       }
+       return super.withdraw(amount);
+   }
+
+    @Override
+    public boolean deposit(double amountOfMoney) {
+        if(amountOfMoney<100){
+            System.out.println("error amount should be > 100");
+            return false;
+        }
+        return super.deposit(amountOfMoney);
+    }
+
+    @Override
+    public void view() {
+        System.out.println("Savings Bank Account");
+        System.out.println("account ID : "+accountId);
+        System.out.println("balance : "+balance);
+        System.out.println("min Balance : "+minimumBalance);
+    }
+
+   
+   
+    
+}
