5e5ca757d8f41693d030ac138951816cf8b88b92
Abd Alrhman Allam <62191765+AbdAlrhmanAllam98@users.noreply.github.com>
2021-01-27 23:13:04 +0200

    Update SavingsBankAccount.java

--- src/BankingSystem/SavingsBankAccount.java
+++ src/BankingSystem/SavingsBankAccount.java
@@ -1,29 +1,46 @@
 package BankingSystem;
 
 public class SavingsBankAccount extends BankAccount {
- 
+
     double minimumBalance;
 
     public SavingsBankAccount(double minimumBalance, double balance) {
-        super(balance);
-        this.minimumBalance = minimumBalance;        
-   }
-   public SavingsBankAccount(){
-       super(1000);
-       minimumBalance=1000;
-   }
+        this.minimumBalance = minimumBalance;
+        accountId = IDS_GENERATOR++;
+        if (balance >= minimumBalance) {
+            super.balance = balance;
+        } else {
+            System.out.println(" The minimum of amount should be greater than Minimum Balance ");
+        }
+    }
+
+    public SavingsBankAccount(double balance) {
+        minimumBalance = 1000;
+        accountId = IDS_GENERATOR++;
+        if (balance >= minimumBalance) {
+            super.balance = balance;
+        } else {
+            System.out.println(" The minimum of amount should be greater than Minimum Balance ");
+        }
+    }
+
+    public SavingsBankAccount() {
+        super(1000);
+        minimumBalance = 1000;
+    }
+
     @Override
-   public boolean withdraw(double amount){
-       if(balance-amount<minimumBalance){
-           System.out.println("error invalid input");
-           return false;
-       }
-       return super.withdraw(amount);
-   }
+    public boolean withdraw(double amount) {
+        if (balance - amount < minimumBalance) {
+            System.out.println("error....invalid input");
+            return false;
+        }
+        return super.withdraw(amount);
+    }
 
     @Override
     public boolean deposit(double amountOfMoney) {
-        if(amountOfMoney<100){
+        if (amountOfMoney < 100) {
             System.out.println("error amount should be > 100");
             return false;
         }
@@ -33,12 +50,10 @@ public class SavingsBankAccount extends BankAccount {
     @Override
     public void view() {
         System.out.println("Savings Bank Account");
-        System.out.println("account ID : "+accountId);
-        System.out.println("balance : "+balance);
-        System.out.println("min Balance : "+minimumBalance);
+        System.out.println("account ID : " + accountId);
+        System.out.println("balance : " + balance);
+        System.out.println("min Balance : " + minimumBalance);
+        owner.view();
     }
 
-   
-   
-    
 }
